ff7b014508ff40ad83ee4700fa4bccf1c113718e
Moses Ogwo <mosesogwo@gmail.com>
2019-10-02 14:22:32 +0100

    indent lines in rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,24 +65,23 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
-    end
+  end
     
-    config.before(:each) do
+  config.before(:each) do
     DatabaseCleaner.strategy = :transaction
-    end
-    
-    config.before(:each, js: true) do
+  end
+  
+  config.before(:each, js: true) do
     DatabaseCleaner.strategy = :truncation
-    end
-    
-    # This block must be here, do not combine with the other `before(:each)` block.
-    # This makes it so Capybara can see the database.
-    config.before(:each) do
+  end
+  
+  # This block must be here, do not combine with the other `before(:each)` block.
+  # This makes it so Capybara can see the database.
+  config.before(:each) do
     DatabaseCleaner.start
-    end
-    
-    config.after(:each) do
+  end
+  
+  config.after(:each) do
     DatabaseCleaner.clean
-    end
-
+  end
 end
