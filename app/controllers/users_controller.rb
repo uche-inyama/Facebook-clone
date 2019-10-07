7f38b4e7d1a07627b1632b66bbfa07eede950ea6
Moses Ogwo <mosesogwo@gmail.com>
2019-10-07 15:26:52 +0100

    add create new post to users profile, modify flash display in application layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,11 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
   def show
     @user = User.find_by(id: params[:id])
     @posts = @user.posts
+    @post = Post.new
   end
+
 end
