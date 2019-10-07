f1ea642b317c14f215ea0d17b011046ff01b9660
Moses Ogwo <mosesogwo@gmail.com>
2019-10-07 14:46:29 +0100

    add posts new and create actions, add nav links to application layout

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,37 @@
 class PostsController < ApplicationController
+    before_action :authenticate_user
+    
     def index
-        @post = Post.new
+      @posts = current_user.posts
     end
 
+    def new
+      @post = Post.new
+    end
+
+    def create
+      @post = current_user.posts.build(post_params)
+      if @post.save
+        flash[:success] = 'Post successfully created'
+        redirect_to posts_path
+      else
+        flash.now[:danger] = 'Post cannot be blank'
+        render 'new'
+      end
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def post_params
+      params.require(:post).permit(:body)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    def show
-        
-    end
+  def show
+      
+  end
 end
