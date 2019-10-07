29f17e0482bc6b0bf917671595e3248e1803eb20
Moses Ogwo <mosesogwo@gmail.com>
2019-10-07 15:09:45 +0100

    add gravatar and profile info to user show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-    before_action :authenticate_user
-    
+    before_action :authenticate_user!
+
     def index
       @posts = current_user.posts
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
   def show
-      
+    @user = User.find_by(id: params[:id])
+    @posts = @user.posts
   end
 end
