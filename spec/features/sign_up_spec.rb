49ec3d19379b39cc0f2b5aa5458e84383d10ad76
uche-inyama <depoulet@yahoo.com>
2019-10-08 11:45:46 +0100

    add failing feature test for sign_up

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -13,4 +13,17 @@ RSpec.feature 'sign_up' do
         click_button 'Sign up'
         expect(page).to have_content 'Welcome! You have signed up successfully.'
     end
+
+    it 'does not sign me up, with invalid parameters' do
+        visit '/users/sign_up'
+        fill_in 'First name', with: ' '
+        fill_in  'Surname', with: ''
+        fill_in  'Email', with: ''
+        fill_in  'Password', with: ''
+        fill_in  'Password confirmation', with: ''
+        page.find('#user_date_of_birth').set('')
+        select('Male', from: 'user_gender')
+        click_button 'Sign up'
+        expect(page).to have_content "5 errors prohibited this user from being saved"
+    end
 end
