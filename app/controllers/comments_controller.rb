96251bc4cf9228e8692f674cb389694d9e9da981
Moses Ogwo <mosesogwo@gmail.com>
2019-10-10 11:12:08 +0100

    Complete create action for comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,42 @@
+class CommentsController < ApplicationController
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @post = Post.find_by(id: comment_params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to posts_path
+    else
+      redirect_to posts_path
+    end
+  end
+
+  def edit
+    @comment  = Comment.find_by(id: params[:id])
+  end
+
+  def update
+    @comment = Comment.find_by(id: params[:id])
+    if @comment.update(comment_params)
+      redirect_to posts_path
+    else
+      flash.now[:danger] = 'Edit post failed'
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @comment  = Comment.find_by(id: params[:id])
+    @comment.delete
+    redirect_to posts_path
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:body, :post_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
   resources :users, only: [:index, :show]
   resources :posts
+  resources :comments
 end
