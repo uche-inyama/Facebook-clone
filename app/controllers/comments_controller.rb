b0aff0880fb6add9263e121f0eaf049010b2e11c
Moses Ogwo <mosesogwo@gmail.com>
2019-10-11 15:12:36 +0100

    Modify view for user_show and posts_indes, fix rubocop errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
-class CommentsController < ApplicationController
+# frozen_string_literal: true
 
+class CommentsController < ApplicationController
   def new
     @comment = Comment.new
   end
@@ -8,34 +9,29 @@ class CommentsController < ApplicationController
     @post = Post.find_by(id: comment_params[:post_id])
     @comment = @post.comments.build(comment_params)
     @comment.user = current_user
-    if @comment.save
-      redirect_to request.referrer
-    else
-      redirect_to request.referrer
-    end
+    @comment.save
+    redirect_to request.referrer
   end
 
   def edit
-    @comment  = Comment.find_by(id: params[:id])
+    @comment = Comment.find_by(id: params[:id])
   end
 
   def update
     @comment = Comment.find_by(id: params[:id])
-    if @comment.update(comment_params)
-      redirect_to posts_path
-    else
-      redirect_to posts_path
-    end
+    @comment.update(comment_params)
+    redirect_to posts_path
   end
 
   def destroy
-    @comment  = Comment.find_by(id: params[:id])
+    @comment = Comment.find_by(id: params[:id])
     @comment.delete
     redirect_to request.referrer
   end
 
   private
-    def comment_params
-      params.require(:comment).permit(:body, :post_id)
-    end
+
+  def comment_params
+    params.require(:comment).permit(:body, :post_id)
+  end
 end
