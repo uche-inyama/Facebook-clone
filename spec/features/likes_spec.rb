57502fbbf55a04d7b4eb981bac2d5fb6ef9765d0
Moses Ogwo <mosesogwo@gmail.com>
2019-10-11 14:47:29 +0100

    add liking a comment test for likes feature test

--- spec/features/likes_spec.rb
+++ spec/features/likes_spec.rb
@@ -5,31 +5,19 @@ RSpec.feature 'Likes' do
     scenario 'liking a post' do
         register_user('uac5@gmail.com')
         create_post
-        click_on 'Like'
-        expect(page).to have_content('1 Likes')
-        click_on 'Like'
+        click_button('Like', match: :first)
+        expect(page).to have_content('1 Like')
+        click_button('Like', match: :first)
         expect(page).to have_content('0 Likes')
     end
 
     scenario 'liking a comment' do
-        register_user('uac5@gmail.com')
-        create_post
+        visit posts_path
+        sign_in_user('uac5@gmail.com')
         comment_post
-        click_on 'Like'
-        expect(page).to have_content('1 Likes')
-        click_on 'Like'
-        expect(page).to have_content('0 Likes')
+        within('#comment_div', match: :first) { click_on 'Like' }
+        within('#comment_div', match: :first) { expect(page).to have_content('1 Like') }
+        within('#comment_div', match: :first) { click_on 'Like' }
+        within('#comment_div', match: :first) { expect(page).to have_content('0 Likes') }
     end
 end
-
-
-# scenario "Signing in as another user" do
-#     visit '/sessions/new'
-#     within("#session") do
-#       fill_in 'Email', with: other_user.email
-#       fill_in 'Password', with: other_user.password
-#     end
-#     click_button 'Sign in'
-#     expect(page).to have_content 'Invalid email or password'
-#   end
-# end
