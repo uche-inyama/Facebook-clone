c4366afab79bffcd3baff1923907d4ce07d4bc13
Moses Ogwo <mosesogwo@gmail.com>
2019-10-14 14:34:21 +0100

    add test for confirm a friend and friend requests in friendship_spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,8 @@ class User < ApplicationRecord
     friends_array = friendships.map{|friendship| 
     friendship.friend if friendship.confirmed}
 
-    friends_array + inverse_friendships.map{|friendship|
-    friendship.user if friendship.confirmed}
+    friends_array = friends_array + inverse_friendships.map{ |friendship|
+    friendship.user if friendship.confirmed }
 
     friends_array.compact
   end
@@ -27,7 +27,7 @@ class User < ApplicationRecord
   end
 
   def confirm_friend(user)
-    friendship = inverse_friendships.find{|friendship| friendshp.user == user}
+    friendship = inverse_friendships.find{|friendship| friendship.user == user}
     friendship.confirmed = true
     friendship.save
   end
--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -10,4 +10,34 @@ RSpec.describe Friendship, type: :model do
       expect(users[0].friends.count).to eq(1)
     end
   end
+
+  describe 'pending_friends' do
+    it 'returns all unconfirmed friendships created by a user' do
+      users = create_list(:user, 4)
+      users[0].friendships.create(friend_id: users[1].id, confirmed: true)
+      users[0].friendships.create(friend_id: users[2].id)
+      users[0].friendships.create(friend_id: users[3].id)
+      expect(users[0].pending_friends.count).to eq(2)
+    end
+  end
+
+  describe 'confirm a friend' do
+    it 'accepts a friendship request' do
+      users = create_list(:user, 2)
+      users[0].friendships.create(friend_id: users[1].id)
+      users[1].confirm_friend(users[0])
+      expect(users[0].friends).to include(users[1])
+      expect(users[1].friends).to include(users[0])
+    end
+  end
+
+  describe 'friendrequests' do
+    it 'returns all unaccepted friendships' do
+      users = create_list(:user, 4)
+      users[1].friendships.create(friend_id: users[0].id)
+      users[2].friendships.create(friend_id: users[0].id, confirmed: true)
+      users[3].friendships.create(friend_id: users[0].id)
+      expect(users[0].friend_requests.count).to eq(2)
+    end
+  end
 end
