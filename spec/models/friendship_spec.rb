424f73e829920b4b16393c6d01902d30eb38cfce
Moses Ogwo <mosesogwo@gmail.com>
2019-10-14 12:56:38 +0100

    add user friend test

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
   describe 'friends' do
-    it 'returns all the users friends' do
-
+    it 'returns all the users friends - confirmed friendships' do
+      users = create_list(:user, 4)
+      users[0].friendships.create(friend_id: users[1].id, confirmed: true)
+      users[0].friendships.create(friend_id: users[2].id)
+      users[0].friendships.create(friend_id: users[3].id)
+      expect(users[0].friends.count).to eq(1)
     end
   end
 end
