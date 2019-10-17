3d32c556abfe5ab8c8b2e81c46e1e103cb253a74
uche-inyama <depoulet@yahoo.com>
2019-10-17 11:53:09 +0100

    add test to validate presence and uniqueness of a friend request

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable
+  devise   :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :validatable, :omniauthable
   validates_presence_of :first_name, :surname, :date_of_birth, :gender
   has_many :posts
   has_many :comments
   has_many :likes
+
   has_many :friend_requests, dependent: :destroy
   has_many :pending_friends, through: :friend_requests, source: :friend
 
