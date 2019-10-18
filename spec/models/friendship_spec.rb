b25c6a1b4f1b4be8c3954f5d575a6f2a767ed484
uche-inyama <depoulet@yahoo.com>
2019-10-18 10:23:33 +0100

    add unit test for presence & uniqueness validations for friend and a user

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -2,6 +2,47 @@ require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
  
+  describe 'validation' do
+    it 'ensures presence of a user' do
+      @two_users = create_pair(:user)
+      @request = Friendship.new(user: nil, friend: @two_users[1])
+      expect(@request.save).to eq(false)
+    end
+
+    it 'ensures presence of friend' do
+      @two_users = create_pair(:user)
+      @request = Friendship.new(user: @two_users[0], friend: nil)
+      expect(@request.save).to eq(false)
+    end
+  end
+
+  describe 'uniqueness of friend' do
+    context 'friendship from same user' do
+      it 'ensures uniqueness of friend for a particular user' do
+        @two_users = create_pair(:user)
+        @request1 = Friendship.create(user: @two_users[0], friend: @two_users[1])
+        @request2 = Friendship.new(user: @two_users[0], friend: @two_users[1])
+        expect(@request2.save).to eq(false)
+      end
+    end
+
+    context 'friendship from a different user' do
+      it 'ensures uniqueness of friend for a particular user' do
+        @users = create_list(:user, 3)
+        @request1 = Friendship.create(user: @users[0], friend: @users[1])
+        @request2 = Friendship.new(user: @users[2], friend: @users[1])
+        expect(@request2.save).to eq(true)
+      end
+    end
+
+    context 'friendship from a user to himself' do
+      it "ensures a friend can't befriend himself " do
+        @users = create_list(:user, 1)
+        @request1 = Friendship.new(user: @users[0], friend: @users[0])
+        expect {@request1.save!}.to  raise_error(ActiveRecord::RecordInvalid, "Validation failed: Friend Can't befriend yourself")
+      end
+    end
+  end
 
   describe 'Associations' do
     it 'belongs to a user' do
