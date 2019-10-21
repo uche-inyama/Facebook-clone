19e0c83df1c9ff3bd90a0a36739fe2ca649163d4
uche-inyama <depoulet@yahoo.com>
2019-10-21 15:11:38 +0100

    resolve conflict in route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,9 @@
 Rails.application.routes.draw do
   
   root to: 'posts#index'
-<<<<<<< HEAD
-<<<<<<< HEAD
+
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  devise_for :users, path: '', 
-  path_names: { sign_in: 'login', sign_out: 'logout'}, 
-  :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
->>>>>>> parent of 61603b4... mutual friendship all ready in place
-  
-=======
->>>>>>> parent of dfbf9b1... setting up omniauth-facebook
-=======
->>>>>>> parent of dfbf9b1... setting up omniauth-facebook
-=======
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
->>>>>>> parent of dfbf9b1... setting up omniauth-facebook
+
   resources :users, only: [:index, :show]
   resources :posts
   resources :comments
