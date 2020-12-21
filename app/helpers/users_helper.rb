a69f4efb40bb8d591e53aaf3015a762eb81669f4
uche-inyama <depoulet@yahoo.com>
2020-12-21 18:01:58 +0100

    refactor view files, to remove logic

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,10 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.first_name, class: 'gravatar')
   end
+
+  def signed_in_create_post?(user, current_user, post)
+    if user == current_user
+      render 'users/post_form'
+    end
+  end
 end
